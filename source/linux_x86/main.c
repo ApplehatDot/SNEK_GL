49b790f03e6164a56b5f0f665c7627f8614333da
ApplehatDoesStuff <152534245+ApplehatDot@users.noreply.github.com>
2024-07-22 20:18:38 +0200

    Update main.c

--- source/linux_x86/main.c
+++ source/linux_x86/main.c
@@ -1,7 +1,7 @@
 /*
  * SNEK - port SNAKE II dla systemów GNU/Linux x86
  *
- * Przeportowany przez J. "Krecika" K. - na licencji MIT
+ * Przeportowany przez Applehat. ("ApplehatDoesStuff") - na licencji MIT
  * Biblioteki użyte do tego portu:
  * - GTK+ 2  [libgtk2.0-dev]
  * - freeglut [freeglut3, freeglut3-dev]
@@ -52,7 +52,7 @@ bool debugMode = false;
 int PointCount, LiczbaSegmentow = 1;
 int show_xy;
 
-char MITLicense[] = "SNEK - Snake II dla systemow rodziny GNU/Linux x86\nProjekt Prywatny - Napisany przez J. 'Krecika' K.\nPROJEKT NIE JEST WYSYŁANY POD PUBLIKE, PODLEGA NA LICENCJI MIT:\n\nCopyright 2024 ApplehatDoesStuff\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ,,Software''), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED,,AS IS'', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
+char MITLicense[] = "SNEK - Linux Release version of SNEK\nCreated by Applehat ('ApplehatDoesStuff') - Project is distributed under MIT License:\n\nCopyright 2024 ApplehatDoesStuff\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ,,Software''), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED,,AS IS'', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
 
 
 // dla ułatwienia robocizny, funkcja tworząca messagebox w GTK+
