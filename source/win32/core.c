3eaabda44be06ee1a68306347a607988c0c1569b
ApplehatDoesStuff <152534245+ApplehatDot@users.noreply.github.com>
2024-07-23 20:41:06 +0200

    Update core.c

--- source/win32/core.c
+++ source/win32/core.c
@@ -12,7 +12,7 @@
 //(* - niegotowe; V - zaimplementowane; T - w trakcie testowania;)//
 /** [V] dodaj sekretną opcję użycia config-(nazwa).ini **/
 /** [*] dodaj opcje w pliku config.ini na zmiane rozdzielczości **/
-/** [T] dodaj punktacje (PointCount) do dialogu przegranej **/
+/** [V] dodaj punktacje (PointCount) do dialogu przegranej **/
 
 #include <stdio.h>
 #include <stdbool.h>
@@ -59,7 +59,7 @@ bool debugMode = false;
 //definicja globalnych zmiennych (dla glutInit)
 int global_argc;
 char **global_argv;
-
+char PointCountChar[100];
 char MITLicense[] = "SNEK - Win32 Release version of SNEK\nCreated by Applehat (ApplehatDoesStuff) - Project is distributed under MIT License:\n\nCopyright 2024 ApplehatDoesStuff\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ,,Software''), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED,,AS IS'', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
 
 
@@ -235,6 +235,11 @@ void Timer(int value) {
     float oldDotX = DotX;
     float oldDotY = DotY;
 
+    //Dodaj ilość PointCount [TODO - #3]
+    char buffer[50];    //bufor tymczasowy dla napisu
+    sprintf(buffer, "Detected Wall Collision! - You Lost!\nPoints scored: %d", PointCount);    //do buforu: komunikat i liczba
+    strcpy(PointCountChar, buffer);    //kopiuje to co z buforu na PointCountChar
+    
     // Zapisz poprzednią pozycję głowy węża
     float prevX = DotX;
     float prevY = DotY;
@@ -245,14 +250,15 @@ void Timer(int value) {
 
     // Sprawdź kolizję z wężem
     if (KolizjaWeza(DotX, DotY)) {
-        MessageBoxA(NULL, "Detected Wall collision - You lost! (awh man :[ )", "Game Over", MB_OK);
+        MessageBoxA(NULL, PointCountChar, "Game Over", MB_OK);
         exit(0);
     }
 
     // Sprawdź kolizję z jedzeniem i dodaj nowy segment
     if (fabs(DotX - FoodX) <= limit && fabs(DotY - FoodY) <= limit) {
         DodajSegmentWeza(prevX, prevY);  // Dodaj nowy segment na poprzedniej pozycji głowy
-        GenerateFood();  // Wygeneruj nowe jedzenie
+        PointCount += 100;    // jedno zdobycie jedzenia to 100 punktów.
+        GenerateFood();
     }
 
     // Przesuń każdy segment węża na miejsce poprzedniego
