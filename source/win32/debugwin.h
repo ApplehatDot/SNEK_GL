d5245494291a343d4aba9e24c6b9bac62634020c
ApplehatDoesStuff <152534245+ApplehatDot@users.noreply.github.com>
2024-07-28 18:39:03 +0200

    Delete source/win32/debugwin.h

--- source/win32/debugwin.h
+++
@@ -1,9 +0,0 @@
-#ifndef DEBUGWIN_H
-#define DEBUGWIN_H
-
-#include <windows.h>
-
-void CreateDebugWindow(HINSTANCE hInstance, int nCmdShow);
-void UpdateDebugWindow(float DotX, float DotY, float FoodX, float FoodY, int LiczbaSegmentow);
-
-#endif // DEBUGWIN_H
