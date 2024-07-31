d3bfb56813e8083753a85a1d1d0879aa9cb053b4
ApplehatDoesStuff <152534245+ApplehatDot@users.noreply.github.com>
2024-07-31 18:33:38 +0200

    add translation header files

---
+++ source/win32/translations/translation_cz.h
@@ -0,0 +1,19 @@
+/*
+ * 		translation.h file - Czech Translation packet for SNEK
+ * 		Big thanks to tbifo_official!
+ * 		Prototype solution for translations
+ */
+ 
+#ifndef TRANSLATION_H
+#define TRANSLATION_H
+
+#define about_dialog_title L"About MEGC project - SNEK_GL"
+#define about_dialog_caption L"SNEK - Win32 version of MEGC project\nv1.0.0.1-insiders\n\nVytvořil Applehat (ApplehatDoesStuff)\nČeský Překlady poskytuje tbifo_official\n\nProjekt SNEK_GL je distribuován pod Licence MIT.\n\nCopyright (C) 2024 ApplehatDoesStuff\n"
+
+#define Pause_dialog_title L"Pozastaveno!"
+#define Pause_dialog_caption L"Hra byla pozastavena, pro návrat stiskněte OK nebo zavřete dialogové okno Messagebox (to jsem já!)"
+
+#define Points_scored_caption L"Zjištěna Kolize Zdi! - Prohráli jste!\nSkóre bodů: %d"
+#define Points_scored_title L"Konec Hry ( Ó ne:[ )"
+
+#endif //translation.h
---
+++ source/win32/translations/translation_pl-PL.h
@@ -0,0 +1,18 @@
+/*
+ * 		translation.h file - Polish Translation packet for SNEK 
+ * 		Prototype solution for translations
+ */
+ 
+#ifndef TRANSLATION_H
+#define TRANSLATION_H
+
+#define about_dialog_title L"O Projekcie MEGC - SNEK_GL"
+#define about_dialog_caption L"SNEK - Wersja dla Win32 projektu MEGC\nv1.0.0.1-insiders\n\nStworzone przez Applehat (ApplehatDoesStuff)\nPolskie Tłumaczenie dostarczone przez applehat.\nSNEK_GL Project is distributed under MIT License.\n\nCopyright (C) 2024 ApplehatDoesStuff\n"
+
+#define Pause_dialog_title L"Pauza!"
+#define Pause_dialog_caption L"Gra została zatrzymana, Naciśnij OK albo zamknij dialog Messagebox (to ja!) by wznowić"
+
+#define Points_scored_caption L"Wykryto kolizje! - Przegrana!\nilość zebranych punktów: %d"
+#define Points_scored_title L"Przegrana (a to pech...)"
+
+#endif //translation.h
