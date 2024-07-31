4ef6e1dedf2b7222b4da76cd447b8ddbc7f226cd
ApplehatDoesStuff <152534245+ApplehatDot@users.noreply.github.com>
2024-07-31 16:53:03 +0200

    add translation header (translations prototype)

---
+++ source/win32/translation_en-US.h
@@ -0,0 +1,18 @@
+/*
+ * 		translation.h file - English (US) Translation packet for SNEK 
+ * 		Prototype solution for translations
+ */
+ 
+#ifndef TRANSLATION_H
+#define TRANSLATION_H
+
+#define about_dialog_title L"About MEGC project - SNEK_GL"
+#define about_dialog_caption L"SNEK - Win32 version of MEGC project\nv1.0.0.1-insiders\n\nCreated by Applehat (ApplehatDoesStuff)\nEnglish (US) Translations Provided by applehat.\nSNEK_GL Project is distributed under MIT License.\n\nCopyright (C) 2024 ApplehatDoesStuff\n"
+
+#define Pause_dialog_title L"Pause!"
+#define Pause_dialog_caption L"Game has been Paused, Press OK or close Messagebox dialog (that's me!) to return"
+
+#define Points_scored_caption L"Detected Wall Collision! - You lost!\nPoints Scored: %d"
+#define Points_scored_title L"Game Over ( awh man :[ )"
+
+#endif //translation.h
