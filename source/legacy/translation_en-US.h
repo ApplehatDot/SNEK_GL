b904f61ef2313f2c75d2b287f996aef16c54f742
ApplehatDoesStuff <152534245+ApplehatDot@users.noreply.github.com>
2024-10-25 18:00:00 +0200

    Add Legacy version

---
+++ source/legacy/translation_en-US.h
@@ -0,0 +1,14 @@
+/*
+ * 		translation.h file - English (US) Translation packet for SNEK_legacy
+ */
+ 
+#ifndef TRANSLATION_H
+#define TRANSLATION_H
+
+#define Pause_dialog_title L"Pause!"
+#define Pause_dialog_caption L"Game has been Paused, Press OK or close Messagebox dialog (that's me!) to return"
+
+#define Points_scored_caption L"Detected Wall Collision! - You lost!\nPoints Scored: %d"
+#define Points_scored_title L"Game Over ( awh man :[ )"
+
+#endif //translation.h
